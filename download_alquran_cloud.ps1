92ed0cf0241097815472b426c69358352977573f
adlmii <aidilfahmi601@gmail.com>
2026-02-16 19:35:16 +0700

    Remove PowerShell data processing scripts

--- download_alquran_cloud.ps1
+++
@@ -1,9 +0,0 @@
-$dest = "app/src/main/assets"
-
-Write-Host "Downloading Tajweed Data..."
-curl.exe -L -o "$dest/quran_tajweed_full.json" "http://api.alquran.cloud/v1/quran/quran-tajweed"
-
-Write-Host "Downloading Translation Data..."
-curl.exe -L -o "$dest/quran_translation_full.json" "http://api.alquran.cloud/v1/quran/id.indonesian"
-
-Write-Host "Download Complete."
